2dc9e4b4b74a00d656b76de50eb6e73879a9e347
TofuNotHere <568115411@qq.com>
2015-05-19 14:34:34 +0800

    Hello World C#
    Hello World C# Version

---
+++ Hello_World_C#.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello_World_CSharp
+{
+    class Hello_World_CSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello_World!");
+        }
+    }
+}
